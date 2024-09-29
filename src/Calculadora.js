b902de019675477a8952bc4bd82ffda98f25acbe
Daniel Souza <daniel.souza@iesb.edu.br>
2024-09-29 19:31:05 -0300

    TDD Método soma da calculadora

--- src/Calculadora.js
+++ src/Calculadora.js
@@ -19,8 +19,11 @@ class Calculadora {
     this.#resultado = numero;
   }
 
-  soma() {
-    
+  soma(numero) {
+    if(typeof numero !== 'number') {
+      throw new TypeError("O argumento deve ser um número válido");
+    }
+    this.#resultado = this.#resultado + numero;
   }
 
   divisao(numero) {
